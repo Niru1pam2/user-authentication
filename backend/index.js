89d0216378622371c8ef3a4f2e435fcb3808bf10
Nirupam Talukdar <152910199+Niru1pam2@users.noreply.github.com>
2024-11-18 11:38:37 +0530

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -20,6 +20,7 @@ const corsOptions = {
 app.use(cors(corsOptions));
 app.use(bodyParser.json());
 app.use("/auth", AuthRouter);
+app.options('*', cors(corsOptions));
 app.use("/products", ProductRouter);
 
 app.listen(PORT, () => {
