beb8ec7f54af86c8235e29188ad30ba83c1e4e3d
Nirupam Talukdar <152910199+Niru1pam2@users.noreply.github.com>
2024-11-18 11:35:50 +0530

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -9,9 +9,16 @@ require("dotenv").config();
 require("./models/db");
 const PORT = process.env.PORT || 8080;
 
+const corsOptions = {
+  origin: "https://user-authentication-jxfvax8wr-niru1pam2s-projects.vercel.app", // Frontend domain
+  methods: ["GET", "POST", "PUT", "DELETE"], // Allowed HTTP methods
+  allowedHeaders: ["Content-Type", "Authorization"], // Allowed headers
+  credentials: true, // If you're using cookies or authorization headers
+};
+
 //middlewares
+app.use(cors(corsOptions));
 app.use(bodyParser.json());
-app.use(cors());
 app.use("/auth", AuthRouter);
 app.use("/products", ProductRouter);
 
