11aec5a256e76b8d07161309cb6539aa2222b14c
Nirupam Talukdar <152910199+Niru1pam2@users.noreply.github.com>
2024-11-18 11:19:06 +0530

    Update Home.js

--- frontend/src/pages/Home.js
+++ frontend/src/pages/Home.js
@@ -21,7 +21,7 @@ function Home() {
 
   const fetchProducts = async () => {
     try {
-      const url = "http://localhost:8080/products";
+      const url = "https://user-authentication-7cj0feiks-niru1pam2s-projects.vercel.app/products";
       const headers = {
         headers: {
           Authorization: localStorage.getItem("token"),
